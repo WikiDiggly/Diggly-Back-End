1e9c46d42eb28119e6876e981bf59d1b6f168a8e
ola-halima <olapade.halima@gmail.com>
2016-02-16 19:04:30 -0500

    added first test using mock

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,15 @@
+import os
+import sys
+import mock
 import pytest
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../diggly'))
+#import diggly.views as Diggly_Views
+from diggly.models import Topic, TopicLink
 from util.django_util import Django_Helper
+from django.http import Http404
+
 
 django_mgt = Django_Helper()
 
@@ -16,3 +26,27 @@ class TestAPIClass:
 
 		for stringOne, stringTwo in routes:
 			assert (stringOne == stringTwo)
+
+	def test_list_topic(self):
+		with mock.patch('diggly.models.Topic') as Topic_mock:
+			from diggly.views import get_topic_by_id
+			Topic.objects = mock.Mock()
+			#Topic_mock.objects = mock.Mock()
+			#Topic_mock.objects.configure_mock(**conf)
+			request_context = mock.MagicMock()
+
+			conf = {'get.side_effect': Topic.DoesNotExist}
+			Topic.objects.configure_mock(**conf)
+			
+			with pytest.raises(Http404):
+				get_topic_by_id(request_context, 1)
+
+
+
+	#helper functions
+	def __create_mock_Topic(self):
+		mock_instance = mock.Mock(spec=Topic)
+		mock_instance.article_id = 123
+		mock_instance.article_title = "TestTopic1"
+
+		return mock_instance
