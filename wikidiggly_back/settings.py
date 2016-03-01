5a74a018040e1aff40f647fbb3ea1be6067a9505
Brandon Him <brh55@drexel.edu>
2016-03-01 18:26:48 -0500

    Whitelist diggly firebase

--- wikidiggly_back/settings.py
+++ wikidiggly_back/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = (
     'django_mongodb_engine',
     'rest_framework',
     'diggly',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = ('diggly.firebaseio.com')
