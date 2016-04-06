22077e562a9a820204e2ad7722217d34f6c3a814
ola-halima <olapade.halima@gmail.com>
2016-04-05 20:18:27 -0400

    first working implementation of cosine similarity calculation

--- diggly/util/wikipediaAPI/wiki_api.py
+++ diggly/util/wikipediaAPI/wiki_api.py
@@ -54,7 +54,7 @@ class WikipediaHelper():
         linked_titles = self.api_utils.parse_linked_pages(tid, pages)
 
         # select 6 random topics
-        #rel_links = self.fetch_relevant_topics(linked_titles, num_linked_topics)
+        # rel_links = self.fetch_relevant_topics(linked_titles, num_linked_topics)
         rel_links = self.fetch_relevant_topics(linked_titles, max_linked_topics)
         source_topic.outlinks = linked_titles
         all_linked_topics = []
@@ -64,34 +64,51 @@ class WikipediaHelper():
 
         try:
             thread_count = 1
-            topic_links = []
             topic_desc_dict = {}
             threads = []
+            rel_topics = []
 
             topic_desc_dict.update({source_topic.article_id: source_topic.description})
 
-            for i in range (1, MAX_NUM_THREADS+1):
-                tmp_thread = FuncThread(thread_count, self.spun_topic_creator,
-                                        source_topic, rel_links, topic_links, topic_desc_dict)
+            #spun threads to create topic objects
+            for i in range(1, MAX_NUM_THREADS + 1):
+                tmp_thread = FuncThread(thread_count, self.spun_topic_creator, rel_links,
+                                        rel_topics, topic_desc_dict)
                 tmp_thread.start()
                 threads.append(tmp_thread)
-                print "Spun Thread #", thread_count, "\n"
+                print "Spun Topic Creator Thread #", thread_count, "\n"
                 thread_count += 1
 
             # wait for threads to return
             for t in threads:
                 t.join()
 
-            #print "Exiting Main Thread.."
+            print "Continuing Main Thread after topic creation...\n"
 
             # calculate score of related topics
             print "About to start calculating scores\n"
             scored_desc_dict = score_topics(source_topic.article_id, topic_desc_dict)
 
-            print "\nNEW SCORES FOR TOPICS:\n"
-            for a,b in scored_desc_dict.iteritems():
+            for a, b in scored_desc_dict.iteritems():
                 print a, "-->", b, "\n"
+            print "\n Done Computing NEW SCORES FOR TOPICS:\n"
 
+            # spun threads to create topic links
+            del threads[:]
+            topic_links = []
+            for i in range(1, MAX_NUM_THREADS + 1):
+                tmp_thread = FuncThread(thread_count, self.spun_tlink_creator, source_topic,
+                                        scored_desc_dict, rel_topics, topic_links)
+                tmp_thread.start()
+                threads.append(tmp_thread)
+                print "Spun Topic_Link Creator Thread #", thread_count, "\n"
+                thread_count += 1
+
+            # wait for threads to return
+            for t in threads:
+                t.join()
+
+            print "Exiting Main Thread...\n"
             sorted_tl = sorted(topic_links, key=lambda instance: instance.score, reverse=True)
 
             # added like this to prevent Index out of bounds error on sorted_tl
@@ -108,15 +125,15 @@ class WikipediaHelper():
             print "Error: issue with threads"
             print str(E)
 
-    def spun_topic_creator(self, source_topic, rel_topics, topic_links, topic_desc_dict):
+    def spun_topic_creator(self, rel_links, rel_topics, topic_desc_dict):
         topic_name = None
         exit_thread = False
 
         thread_lock1.acquire()
-        if len(rel_topics) == 0:
+        if len(rel_links) == 0:
             exit_thread = True
         else:
-            topic_name = rel_topics.pop()
+            topic_name = rel_links.pop()
         thread_lock1.release()
 
         while not exit_thread and topic_name is not None:
@@ -127,41 +144,63 @@ class WikipediaHelper():
             linked_page = response[PAGE_RESP_KEY]
             redirected_page = response[REDIRECT_RESP_KEY] if REDIRECT_RESP_KEY in response else None
 
-            linked_topic = self.api_utils.parse_and_save_topic(linked_page, redirected_page)[0] # get the first element returned
-
-            # create topiclink relation for linked_topic
-            reltext = linked_topic.article_title + " is linked to " + source_topic.article_title
-            rand_score = self.api_utils.get_rand_score(source_topic, linked_topic)
-            tldata = {"source_id" : source_topic,
-                    "target_id" : linked_topic,
-                    "title" : linked_topic.article_title,
-                    "description" : reltext,
-                    "wiki_link" : linked_topic.wiki_link,
-                    "base_score" : rand_score,
-                    "user_score" : 0.0,
-                    "score": rand_score
-                    }
-
-            tlink = self.tl_creator.create_topiclink(tldata)
-
-            # add to topic_link to list of related topics
-            thread_lock2.acquire()
-            if tlink is not None: # prevent creation of topiclink where source_id == target_id
-                topic_links.append(tlink)
-            thread_lock2.release()
+            linked_topic = self.api_utils.parse_and_save_topic(linked_page, redirected_page)[
+                0]  # get the first element returned
 
             # add to topic_name -> description to dictionary
-            thread_lock3.acquire()
+            thread_lock2.acquire()
             if linked_topic.article_id not in topic_desc_dict:
+                rel_topics.append(linked_topic)
                 topic_desc_dict.update({linked_topic.article_id: linked_topic.description})
+            thread_lock2.release()
+
+            thread_lock1.acquire()
+            if len(rel_links) == 0:
+                exit_thread = True
+            else:
+                topic_name = rel_links.pop()
+            thread_lock1.release()
+
+    def spun_tlink_creator(self, source_topic, scored_desc_dict, rel_topics, topic_links):
+        linked_topic = None
+        exit_thread = False
+
+        thread_lock1.acquire()
+        if len(rel_topics) == 0:
+            exit_thread = True
+        else:
+            linked_topic = rel_topics.pop()
+        thread_lock1.release()
+
+        while not exit_thread and linked_topic is not None:
+            # create topiclink relation for linked_topic
+            rel_text = linked_topic.article_title + " is linked to " + source_topic.article_title
+            tl_score = scored_desc_dict[linked_topic.article_id]
+            tl_data = {"source_id": source_topic,
+                       "target_id": linked_topic,
+                       "title": linked_topic.article_title,
+                       "description": rel_text,
+                       "wiki_link": linked_topic.wiki_link,
+                       "base_score": tl_score,
+                       "user_score": 0.0,
+                       "score": tl_score
+                       }
+
+            tlink = self.tl_creator.create_topiclink(tl_data)
+
+            # add to topic_link to list of related topics
+            thread_lock3.acquire()
+            if tlink is not None:  # prevent creation of topiclink where source_id == target_id
+                topic_links.append(tlink)
             thread_lock3.release()
 
             thread_lock1.acquire()
             if len(rel_topics) == 0:
                 exit_thread = True
             else:
-                topic_name = rel_topics.pop()
+                linked_topic = rel_topics.pop()
             thread_lock1.release()
+
 
     def fetch_relevant_topics(self, title_list, num_res):
         res = []
